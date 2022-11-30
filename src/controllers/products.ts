358d459d972f3e541a393163be581d44118efc94
Yevheniya Deryka <110738010+Yevheniya-Deryka@users.noreply.github.com>
2022-11-30 17:51:06 +0200

    Revert "Added GET /products endpoint returning all phones data in json format"

--- src/controllers/products.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import { Request, Response } from 'express';
-
-export const getAll = async (req: Request, res: Response) => {
-  const products = fs.readFileSync('public/api/phones.json');
-  res.send(products.toString());
-}
--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import serverless from 'serverless-http';
 
-import * as productsController from './controllers/products';
-
 const router = express.Router();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
 router.get('/', (req, res) => {
   res.json({
     'Hello': 'from Ta-y-take_team',
   })
 })
-
-router.get('/products', productsController.getAll);
 
 // For development testing:
 // app.use('/', router);
