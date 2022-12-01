3f06afd5f8f6bc7f5e94fbcaa735dba890dfa7b4
Yevheniya Deryka <evgeniyaderika@gmail.com>
2022-12-01 22:47:36 +0200

    Changed stream to readFile

--- src/controllers/images.ts
+++ src/controllers/images.ts
@@ -2,17 +2,15 @@ import fs from 'fs';
 import { Request, Response } from 'express';
 import path from 'path';
 
-export const getImage = (req: Request, res: Response) => {
+export const getImage = async (req: Request, res: Response) => {
   try {
     const imgLink = req.originalUrl.split('server')[1].slice(1);
 
-    res.setHeader('Content-Type', 'image/jpeg');
-
-    const image = fs.createReadStream(path.resolve(__dirname, imgLink));
-
-    image.pipe(res);
-
+    fs.readFile(path.resolve(__dirname, imgLink), (err, content) => {
+      res.setHeader('Content-Type', 'image/jpeg');
+      res.end(content);
+    });
   } catch (error) {
-    res.sendStatus(400);
+    res.sendStatus(500);
   }
 }
